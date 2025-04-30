4638cf0c98e64a367e6f5fbb1b1a38a9c63796aa
Afriatsya <153706658+Afriatsya@users.noreply.github.com>
2025-04-30 22:08:18 +0800

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+
+  document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault(); 
+
+   
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+ 
+    if (username === "admin" && password === "1234") {
+      window.location.href = "assets/login/beranda.html"; 
+    } else {
+      alert("Username atau password salah!");
+    }
+  });
+
